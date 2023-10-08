f8c173a5aaed62fc728b25a5f62476a6a0b67050
sudipchow <147301185+sudipchow@users.noreply.github.com>
2023-10-08 21:11:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 // Your First Program
-
+// tutorial to add how to write main() function in a c programming.
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
